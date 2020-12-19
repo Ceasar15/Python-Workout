e3615239fb63789414d0dd148e781307e49e60d1
Ceasar15 <ceasarkwadwo@gmail.com>
2020-12-19 10:22:27 +0000

    Inheritance Example

--- InheritanceExample.py
+++ InheritanceExample.py
@@ -32,3 +32,15 @@ class ArithmeticProgression(Progression):
 
 
 ArithmeticProgression(10,85).print_progression(10)
+
+
+
+class FibonacciProgression(Progression):
+    def __init__(self, first=0, second =1):
+        super().__init__(first)
+        self._prev = second - first
+
+    def _advance(self):
+        self._prev,self._current =  self._current, self._prev + self._current
+
+FibonacciProgression(0,1).print_progression(10)
