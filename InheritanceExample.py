1f8b112e23cc6e38005dfaf85d064eead5209ac5
Ceasar15 <ceasarkwadwo@gmail.com>
2020-12-19 12:56:33 +0000

    inheritance examples

--- InheritanceExample.py
+++ InheritanceExample.py
@@ -1,3 +1,6 @@
+from os import times
+import time
+
 class Progression():
     def __init__(self, start=0):
         self._current = start
@@ -19,8 +22,8 @@ class Progression():
     def print_progression(self, n):
         print(' '.join(str(next(self)) for j in range(n)))
 
-
-Progression(3).print_progression(2)
+print('\n Progression: ')
+Progression(3).print_progression(10)
 
 class ArithmeticProgression(Progression):
     def __init__(self, increment=1, start=0):
@@ -30,10 +33,20 @@ class ArithmeticProgression(Progression):
     def _advance(self):
         self._current += self._increment
 
-
+print('\n Arithmetric Progression: ')
 ArithmeticProgression(10,85).print_progression(10)
 
 
+class GeometricProgression(Progression):
+    def __init__(self, base=2,start=1):
+        super().__init__(start)
+        self._base = base
+
+    def _advance(self):
+        self._current *= self._base
+
+print('\n Geometric Progression: ')
+GeometricProgression(10,5).print_progression(10)
 
 class FibonacciProgression(Progression):
     def __init__(self, first=0, second =1):
@@ -43,4 +56,31 @@ class FibonacciProgression(Progression):
     def _advance(self):
         self._prev,self._current =  self._current, self._prev + self._current
 
+print('\n Fiibonacci Progression: ')
 FibonacciProgression(0,1).print_progression(10)
+
+if __name__ == '__main__':
+
+    print('\n Unit Test Loading In 5 secs')
+    time.sleep(5)
+    print('\n Default Progression: ')
+    Progression().print_progression(10)
+
+    print('\n Arithmetic Progression starting from 5')
+    ArithmeticProgression(5).print_progression(10)
+
+    print('\n Arithmetic Progression with increment 5 and start 2')
+    ArithmeticProgression(5,2).print_progression(10)
+
+    print('\n Geometric Progression with default base 2')
+    GeometricProgression().print_progression(10)
+
+    print('\n Geometric Progression with base 3 and start 5')
+    GeometricProgression(3,5).print_progression(10)
+
+    print('\n Fibonnaci Sequence with default start values')
+    FibonacciProgression().print_progression(10)
+
+    print('\n Fibonnaci Sequence with start values 4 and 6')
+    FibonacciProgression(4,6).print_progression(10)
+
