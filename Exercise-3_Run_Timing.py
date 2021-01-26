17a2b8ce3033dce4df2700616d3bd3a8e6a49692
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-26 23:17:19 +0000

    run Timming

--- Exercise-3_Run_Timing.py
+++ Exercise-3_Run_Timing.py
@@ -1,3 +1,18 @@
 def run_timing():
-    try:
-        num = 
+
+    number_of_runs = 0
+    total_time = 0
+
+    while True:
+        try:
+            num = float(input('Enter a number: '))
+            print(f'{num}')
+            if not num:
+                break
+            number_of_runs += 1
+            total_time += float(num)
+        except ValueError:
+            print('That no be good value! ')
+        
+    average_time = total_time/ number_of_runs
+    print
