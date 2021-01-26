f6b1dacef83de9db1f2dc172032090e06019afd8
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-26 23:22:39 +0000

    Run Timing

--- Exercise-3_Run_Timing.py
+++ Exercise-3_Run_Timing.py
@@ -12,6 +12,7 @@ def run_timing():
 
         number_of_runs += 1
         total_time += float(num)
+
         
     average_time = total_time/ number_of_runs
     print(f'Average is {average_time}, over {number_of_runs} runs')
