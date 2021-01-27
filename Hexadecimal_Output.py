1e8ef112eb6eb191b6a6edc607f1b25f8a4ce236
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-27 17:59:01 +0000

    Hexadecimal Output

--- Hexadecimal_Output.py
+++ Hexadecimal_Output.py
@@ -1,8 +1,8 @@
 def hex_output():
     dec_num = 0
-    hex_num = input("Enter your hex number: ")
+    hex_num = input("Enter your number in base 10: ")
     for power, index in enumerate(reversed(hex_num)):
         dec_num += int(index, 16) * (16 ** power)
-    print(f'Hex_output : {hex_num}')
+    print(f'Hex_output : {dec_num}')
 
 hex_output()
