cb324f7003568f598e37e182580ff73e0c23fbf1
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-27 17:57:00 +0000

    Hexadecimal output

--- Hexadecimal_Output.py
+++ Hexadecimal_Output.py
@@ -1,8 +1,8 @@
 def hex_output():
-    while True:
-        hex_num = input("Enter your hex number: ")
-        hex_num ** 2
-        print(f'Hex_output : {hex_num}')
-        break
+    dec_num = 0
+    hex_num = input("Enter your hex number: ")
+    for power, index in enumerate(reversed(hex_num)):
+        dec_num += int(index, 16) * (16 ** power)
+    print(f'Hex_output : {hex_num}')
 
 hex_output()
