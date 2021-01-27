643eab6bfe35e61615d941a8fc411f4c2ae5e8e4
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-27 00:05:04 +0000

    Run Timing

--- Exercise-3_Run_Timing.py
+++ Exercise-3_Run_Timing.py
@@ -1,21 +1,34 @@
-def run_timing():
+# def run_timing():
 
-    number_of_runs = 0
-    total_time = 0
+#     number_of_runs = 0
+#     total_time = 0
 
-    while True:
+#     while True:
 
-        num = input('Enter 10km run time: ')
+#         num = input('Enter 10km run time: ')
         
-        if not num:
-            break
+#         if not num:
+#             break
 
-        number_of_runs += 1
-        total_time += float(num)
+#         number_of_runs += 1
+#         total_time += float(num)
 
         
-    average_time = total_time/ number_of_runs
-    print(f'Average is {average_time}, over {number_of_runs} runs')
+#     average_time = total_time/ number_of_runs
+#     print(f'Average is {average_time}, over {number_of_runs} runs')
 
-run_timing()
+# run_timing()
 
+
+def float_function():
+    fl = float(input("Enter your float: "))
+    before = int(input("Enter a before: "))
+    #after = int(input("Enter a after"))
+
+    print(f'The number is {fl:.2f} ')
+    print(round(fl,before))
+
+    return f'{fl:.2f}'
+
+
+float_function()
