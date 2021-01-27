12585a9481146cffd408725d19e7d86fe873e4aa
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-27 19:07:36 +0000

    hexadecimal

--- Hexadecimal_Output.py
+++ Hexadecimal_Output.py
@@ -1,11 +1,11 @@
-# def hex_output():
-#     dec_num = 0
-#     hex_num = input("Enter your number in base 10: ")
-#     for power, index in enumerate(reversed(hex_num)):
-#         dec_num += int(index, 16) * (16 ** power)
-#     print(f'Hex_output : {dec_num}')
+def hex_output():
+    dec_num = 0
+    hex_num = input("Enter your number in base 10: ")
+    for power, index in enumerate(reversed(hex_num)):
+        dec_num += int(index, 16) * (16 ** power)
+    print(f'Hex_output : {dec_num}')
 
-# hex_output()
+hex_output()
 
 
 def name_triangle():
