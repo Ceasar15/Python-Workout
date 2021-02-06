e3ad4e38bfd5898df35ceb785906695b408a8493
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-06 15:27:19 +0000

    python changes

--- Pig_Latin.py
+++ Pig_Latin.py
@@ -1,5 +1,3 @@
-while True: 
-    name = str(input("Enter your name: "))
-    food = input(f"{name}, What is your favorite food? ")
-    print(f'I like {food} too')
-    break
+name = str(input("Enter your name: "))
+food = input(f"{name}, What is your favorite food? ")
+print(f'I like {food} too')
